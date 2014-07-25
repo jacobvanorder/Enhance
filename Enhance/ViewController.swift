d6675a88d4b50ed5c4b5606af4f326808ad4236d
jake.vanorder@gmail.com <jake.vanorder@gmail.com>
2014-07-24 22:35:37 -0500

    Enhance

--- Enhance/ViewController.swift
+++ Enhance/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var mainImageView: UIImageView!
+    var imageIsEnhanced : Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func enhanceButtonTapped(sender: UIButton) {
+        if !imageIsEnhanced {
+            var coreImage = CIImage(CGImage: UIImage(named: "OffWhite").CGImage)
+            let options = [CIDetectorImageOrientation as String : 1]
+            let adjustments = coreImage.autoAdjustmentFiltersWithOptions(options)
+            for filter :CIFilter in adjustments as [CIFilter] {
+                filter.setValue(coreImage, forKey: kCIInputImageKey);
+                coreImage = filter.outputImage
+            }
+            
+            mainImageView.image = UIImage(CIImage: coreImage)
+            imageIsEnhanced = true
+            /*
+            NSDictionary *options = @{ CIDetectorImageOrientation :
+            [[image properties] valueForKey:kCGImagePropertyOrientation] };
+            NSArray *adjustments = [myImage autoAdjustmentFiltersWithOptions:options];
+            for (CIFilter *filter in adjustments) {
+            [filter setValue:myImage forKey:kCIInputImageKey];
+            myImage = filter.outputImage;
+            }
+*/
+        }
+        else {
+            self.mainImageView.image = UIImage(named:"OffWhite");
+            imageIsEnhanced = false
+        }
+    }
 
 }
 
