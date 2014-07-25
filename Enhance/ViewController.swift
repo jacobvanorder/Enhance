4dcd520a2a848711265eb83245590e30a1c18346
jake.vanorder@gmail.com <jake.vanorder@gmail.com>
2014-07-24 22:03:19 -0500

    Initial Commit

---
+++ Enhance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Enhance
+//
+//  Created by mrJacob on 7/24/14.
+//  Copyright (c) 2014 Vokal Interactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
